8b1b978204675c7245b1556ea1b98a0bb2af8460
Anton <antonchik12@mail.ru>
2022-02-17 18:59:50 +0300

    Первая задача

---
+++ Lesson 1/1.py
@@ -0,0 +1,10 @@
+my_name = 'Антон'
+my_age = 32
+my_city = 'Рязань'
+
+print(my_name, my_age, my_city)
+
+name = input('Введите Ваше имя')
+age = int(input('Введите ваш возраст'))
+city = input('Из какого вы города?')
+print(name, age, city)
