60ec1cdeba62a373da46acfc3c6a26bce39223cf
Anton <antonchik12@mail.ru>
2022-02-17 19:17:11 +0300

    Первая задача

--- Lesson 1/1.py
+++ Lesson 1/1.py
@@ -2,9 +2,9 @@ my_name = 'Антон'
 my_age = 32
 my_city = 'Рязань'
 
-print(my_name, my_age, my_city)
+print('Мое имя', my_name, ', мне', my_age, 'года. Я из города', my_city)
 
 name = input('Введите Ваше имя')
 age = int(input('Введите ваш возраст'))
 city = input('Из какого вы города?')
-print(name, age, city)
+print('Ваше имя', name, '.Вам', age, 'года(лет). Вы из города', city)
